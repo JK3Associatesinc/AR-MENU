d24d13e69aeb1188bb9893a28a50c3f21a40795e
JK3Associatesinc <jk3halton@outlook.com>
2025-06-15 11:27:46 -0400

    Delete src/data directory

--- src/data/menuItems.js
+++
@@ -1,5 +0,0 @@
-export const menuItems = [
-  { id: "pizza", name: "🍕 Pizza", model: "/models/pizza.glb" },
-  { id: "burger", name: "🍔 Burger", model: "/models/burger.glb" },
-  { id: "noodles", name: "🍜 Noodles", model: "/models/noodles.glb" }
-];
