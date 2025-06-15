67bf9b161f6f62f8288b1d4a0e4881d8a39e4daa
JK3Associatesinc <jk3halton@outlook.com>
2025-06-15 11:21:46 -0400

    Create menuItems.js

---
+++ src/data/menuItems.js
@@ -0,0 +1,5 @@
+export const menuItems = [
+  { id: "pizza", name: "🍕 Pizza", model: "/models/pizza.glb" },
+  { id: "burger", name: "🍔 Burger", model: "/models/burger.glb" },
+  { id: "noodles", name: "🍜 Noodles", model: "/models/noodles.glb" }
+];
